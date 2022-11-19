7e64964e3b342185eb4af5bb01b19732ec665806
pdubovskyi <petro.dubovskyi@gmail.com>
2022-11-19 20:13:07 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,56 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+// ------------1----------------
+
+const items = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join('');
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('beforeend', items);
+
+// ---------------2------------------
+galleryEl.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+  console.log(e.target);
+  const origImg = e.target.dataset.source;
+  console.log(origImg);
+  const instance = basicLightbox.create(`
+    <img src="${origImg}">
+`);
+  instance.show();
+
+  window.addEventListener('keydown', onEscapeClick);
+
+  function onEscapeClick(e) {
+    if (e.code === 'Escape') {
+      console.log(e.code);
+      instance.close();
+      onCloseModal();
+    }
+  }
+
+  function onCloseModal() {
+    window.removeEventListener('keydown', onEscapeClick);
+  }
+}
