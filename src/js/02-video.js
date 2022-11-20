607502831815067aa2ff21a6b297ae393e279f83
pdubovskyi <petro.dubovskyi@gmail.com>
2022-11-20 19:34:39 +0200

    Update Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+// console.log(player);
+
+const onPlay = function (event) {
+  // console.log(event.seconds);
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+// console.log(currentTime);
+
+player.setCurrentTime(currentTime).catch(function (error) {
+  switch (error.name) {
+    case 'RangeError':
+      // the time was less than 0 or greater than the video’s duration
+      break;
+
+    default:
+      // some other error occurred
+      break;
+  }
+});
